e18bc813056452c6ab1a32b6e2983fe4aedfe4e3
reagancheatham <reagan.cheatham@eagles.oc.edu>
2025-12-02 23:44:09 -0600

    Remove unnecessary components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import vuetify from "./plugins/vuetify";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(vuetify)
+    .mount("#app");
