8419acb4bac0f003ba539652a1ef5d14bf1013fc
reagancheatham <reagan.cheatham@eagles.oc.edu>
2025-12-04 08:21:33 -0600

    Fix home button

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,6 +1,15 @@
+<script setup>
+import router from "@/routing/router.js";
+import routes from "@/routing/routes.js";
+
+function navigate(name) {
+    router.push(name);
+}
+</script>
+
 <template>
     <v-app-bar>
-        <v-app-bar-nav-icon variant="text" icon="mdi-home"></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon variant="text" icon="mdi-home" @click="navigate(routes.Characters)"></v-app-bar-nav-icon>
         <v-toolbar-title>Deadlock Wiki</v-toolbar-title>
     </v-app-bar>
 </template>
